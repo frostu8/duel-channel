cd04afa9b534fb4ff096d2eb935e29bd0f957f9b
Dante Helmore <frostu8@protonmail.com>
2025-10-26 15:04:48 -0700

    disable login button for now

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -7,7 +7,8 @@
 
 <header>
   <h1>DUEL CHANNEL</h1>
-  {#if !currentUser}
+  <!-- {#if !currentUser} -->
+  {#if false}
   <a class="button" href="https://duelchannel.ringrace.rs/api/v1/users/~redirect">
     <Icon data={faDiscord} scale={1.5}/>
     <p>Login</p>
